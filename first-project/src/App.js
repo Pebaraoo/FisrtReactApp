726af3b78eb944cd02e60ddb2fdaf2af6309707c
Pebaraoo <fbarao15@gmail.com>
2022-03-01 22:04:56 -0300

    Primeiro Projeto com React
    My First React Application

---
+++ first-project/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import HelloWorld from './components/HelloWorld';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1>Olá Reaact</h1>
+    <HelloWorld/>
+    </div>
+  );
+}
+
+export default App;
